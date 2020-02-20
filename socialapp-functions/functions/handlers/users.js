58789db3b1735f9861752cc9587856cf36102b43
shawnSFU <shawn.noruzi@gmail.com>
2020-02-20 14:58:44 -0800

    Backend Hotfix :
    - Fixed filename error when uploaded to DB

--- socialapp-functions/functions/handlers/users.js
+++ socialapp-functions/functions/handlers/users.js
@@ -92,37 +92,36 @@ exports.login = (req, res) => {
     });
 };
 
+// Upload a profile image for user
 exports.uploadImage = (req, res) => {
-  const BusBoy = require("busboy");
-  const path = require("path");
-  const os = require("os");
-  const fs = require("fs");
+  const BusBoy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
 
   const busboy = new BusBoy({ headers: req.headers });
-  let imageFileName;
+
   let imageToBeUploaded = {};
+  let imageFileName;
 
-  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    console.log("fieldname:", fieldname);
-    console.log("filename", filename);
-    console.log("mimetype", mimetype);
-    //grab the image extension
-    //file.name.png
-    const imageExtension = filename.split(".")[filename.split(".").length - 1];
-
-    //grab the filename and concat extension
-    const imageFileName = `${Math.round(
-      Math.random() * 100000000000
-    )}.${imageExtension}`;
-
-    //get filepath
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname, file, filename, encoding, mimetype);
+    if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+      return res.status(400).json({ error: 'Wrong file type submitted' });
+    }
+    // my.image.png => ['my', 'image', 'png']
+    const imageExtension = filename.split('.')[filename.split('.').length - 1];
+    // 32756238461724837.png
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
     const filepath = path.join(os.tmpdir(), imageFileName);
     imageToBeUploaded = { filepath, mimetype };
     file.pipe(fs.createWriteStream(filepath));
   });
-  busboy.on("finish", () => {
+  busboy.on('finish', () => {
     console.log('imageToBeUploaded', imageToBeUploaded)
-    //from fb sdk for fileuploads
+    //from fb sdk for fileuploads   
     admin
       .storage()
       .bucket()
@@ -135,16 +134,17 @@ exports.uploadImage = (req, res) => {
         }
       })
       .then(() => {
-        //alt=media makes it display rather than download
-        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+          config.storageBucket
+        }/o/${imageFileName}?alt=media`;
         return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
       })
       .then(() => {
-        return res.json({ message: "Image uploaded successfully" });
+        return res.json({ message: 'image uploaded successfully' });
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
-        return res.status(500).json({ error: err.code });
+        return res.status(500).json({ error: 'something went wrong' });
       });
   });
   busboy.end(req.rawBody);
