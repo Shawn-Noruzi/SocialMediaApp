bcf0f5b791a6242e72aa07918ea2ecd25dc9bc65
shawnSFU <shawn.noruzi@gmail.com>
2020-02-23 17:03:46 -0800

    Backend:
    -Added a function to mark notifications as being read

--- socialapp-functions/functions/handlers/users.js
+++ socialapp-functions/functions/handlers/users.js
@@ -252,3 +252,20 @@ exports.uploadImage = (req, res) => {
   });
   busboy.end(req.rawBody);
 };
+
+exports.markNotificationsRead = (req, res) => {
+  let batch = db.batch();
+  req.body.forEach((notificationId) => {
+    const notification = db.doc(`/notifications/${notificationId}`);
+    batch.update(notification, { read: true });
+  });
+  batch
+    .commit()
+    .then(() => {
+      return res.json({ message: 'Notifications marked read' });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -43,7 +43,7 @@ app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
 app.get('/user/:handle', getUserDetails);
-// app.post('/notifications', FBAuth, markNotificationsRead);
+app.post('/notifications', FBAuth, markNotificationsRead);
 
 
 exports.api = functions.region('us-central1').https.onRequest(app);
